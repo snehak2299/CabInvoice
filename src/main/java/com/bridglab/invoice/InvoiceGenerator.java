18474c066e5f7c9e475d9740d3f52c9ec4ccc64e
snehak2299 <skanase2299@gmail.com>
2021-09-14 16:21:16 +0530

    [sneha]:calculate fare using distance and time

--- src/main/java/com/bridglab/invoice/InvoiceGenerator.java
+++ src/main/java/com/bridglab/invoice/InvoiceGenerator.java
@@ -2,4 +2,12 @@ package com.bridglab.invoice;
 
 public class InvoiceGenerator {
 
+	private static final int COST_PER_TIME = 1;
+	private static final double MIN_COST_PER_KILOMETER = 10.0;
+
+	public double calculateFare(double distance, int time) {
+		
+		return distance*MIN_COST_PER_KILOMETER+time*COST_PER_TIME;
+	}
+
 }
--- src/test/java/com/bridglab/invoicetest/InvoiceService.java
+++ src/test/java/com/bridglab/invoicetest/InvoiceService.java
@@ -1,12 +1,18 @@
 package com.bridglab.invoicetest;
 
+import org.junit.Assert;
 import org.junit.Test;
-
 import com.bridglab.invoice.InvoiceGenerator;
 
 public class InvoiceService {
+	
 	@Test
-	public void givenDistanceTime_returnFare() {
-		InvoiceGenerator invoiceGeneratornew =new InvoiceGenerator();
+	public void givenDistanceTime_shouldReturnFare() {
+		InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare=invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(25, fare,0.0);
+		
 	}
 }
